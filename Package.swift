ae7fd8ebd0ecb8fbab2e19bdacd45eff4497b0c4
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-08 02:19:56 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RectangleSelector",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RectangleSelector",
-            targets: ["RectangleSelector"]),
+            targets: ["RectangleSelector"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RectangleSelector"),
+            name: "RectangleSelector"
+        ),
         .testTarget(
             name: "RectangleSelectorTests",
-            dependencies: ["RectangleSelector"]),
+            dependencies: ["RectangleSelector"]
+        ),
     ]
 )
--- Tests/RectangleSelectorTests/RectangleSelectorTests.swift
+++ Tests/RectangleSelectorTests/RectangleSelectorTests.swift
@@ -1,12 +1,4 @@
 import XCTest
 @testable import RectangleSelector
 
-final class RectangleSelectorTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
-}
+final class RectangleSelectorTests: XCTestCase {}
